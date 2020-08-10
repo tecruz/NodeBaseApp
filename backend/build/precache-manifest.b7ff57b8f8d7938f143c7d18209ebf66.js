9950a2197e7a831b9b070f7c4d5cca235c5885dd
Tiago Cruz <tiago.cruz@gls-portugal.com>
2020-08-10 14:49:31 +0100

    uibuild

---
+++ backend/build/precache-manifest.b7ff57b8f8d7938f143c7d18209ebf66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b0dd39d90885655ee140c32d5bcb8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a6ceef6ef0032f009eb",
+    "url": "/static/css/main.7776b3fe.chunk.css"
+  },
+  {
+    "revision": "7993db70113e1c1befa4",
+    "url": "/static/js/2.ecceb8a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ecceb8a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6ceef6ef0032f009eb",
+    "url": "/static/js/main.b58c55b2.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
